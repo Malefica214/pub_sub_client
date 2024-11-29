e4ec61ffb2a2b3977081a39aad5b4b8f81757b49
francescazcs <f.buso@zcscompany.com>
2024-11-29 15:11:50 +0100

    correzioni

--- app/main_sub.py
+++ app/main_sub.py
@@ -29,7 +29,7 @@ app = FastAPI(
 log.info(f"Start {app_config['title']} - Subscriber")
 
 MQTT_BROKER = os.environ.get('MQTT_BROKER') or "127.0.0.1"
-MQTT_PORT = os.environ.get('MQTT_PORT') or 9001  
+MQTT_PORT = int(os.environ.get('MQTT_PORT', 9001))
 USERNAME = os.environ.get('USERNAME') or "francesca"
 PASSWORD = os.environ.get('PASSWORD') or "francesca"
 TRANSPORT = os.environ.get('TRANSPORT') or "websockets"
