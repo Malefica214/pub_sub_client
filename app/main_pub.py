61e8e91ab4fd8f5ea97bb2e141eddb5612bfe340
francescazcs <f.buso@zcscompany.com>
2024-11-29 10:29:48 +0100

    log publisher

--- app/main_pub.py
+++ app/main_pub.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Security
+from fastapi import FastAPI, Security, status
+from fastapi.responses import JSONResponse
 import toml
 import publisher
 import logger
@@ -25,7 +26,15 @@ def health():
 
 @app.post("/send-request")
 def send_request_to_queue(message: publisher.Message):
-    publisher.send_request(addresses_id=message.addresses_id,
-                           request=message.request,
-                           sender_id=message.sender_id,
-                           payload=message.model_dump())
+    try:
+        publisher.send_request(addresses_id=message.addresses_id,
+                            request=message.request,
+                            sender_id=message.sender_id,
+                            payload=message.model_dump())
+        return JSONResponse(content={"message": "Messaggio inviato con successo"},
+                        status_code=status.HTTP_200_OK)
+    except Exception as e:
+        log.error(f"Error on publish topic {e}")
+        return JSONResponse(content={"message": "Servizio momentaneamente non disponibile"},
+                        status_code=status.HTTP_503_SERVICE_UNAVAILABLE)
+
--- app/publisher.py
+++ app/publisher.py
@@ -1,6 +1,11 @@
 import paho.mqtt.publish as publish
 from pydantic import BaseModel
 import json
+import os
+
+HOST_NAME = os.environ.get('HOST_NAME') or "127.0.0.1"
+USERNAME = os.environ.get('USERNAME') or "francesca"
+PASSWORD = os.environ.get('PASSWORD') or "francesca"
 
 class Message(BaseModel):
     addresses_id: str
@@ -17,5 +22,5 @@ class Message(BaseModel):
 def send_request(addresses_id: str, request:str, sender_id: str, payload: str):
     print(json.dumps(payload))
     publish.single(f"{addresses_id}/{request}/{sender_id}", payload=json.dumps(payload), qos=1, retain=True, 
-                client_id="diet-match-moble-dev",
-                hostname="127.0.0.1", auth={"username": "francesca", "password": "francesca"})
+                client_id=f"client-{sender_id}",
+                hostname=HOST_NAME, auth={"username": USERNAME, "password": PASSWORD})
